c62a0574111c20fba9bd458f6ffd257bd18c2edf
Richard Hogoboom <rhogoboom@gmail.com>
2022-07-28 09:50:09 -0400

    added basic CSS style to game boards

--- UI.js
+++ UI.js
@@ -2,7 +2,28 @@ const UI = () => {
   const initialLoad = () => {
     const rootNode = document.querySelector(".root");
     const boards = document.querySelectorAll(".board");
+    boards.forEach((board) => {
+      createBoard(board);
+    });
   };
+
+  const createBoard = (board) => {
+    for (let i = 0; i < 10; i++) {
+      const row = document.createElement("div");
+      row.className = "board-row";
+      row.style.width = board.scrollWidth;
+      row.style.height = board.scrollHeight;
+
+      for (let j = 0; j < 10; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("board-cell");
+        row.appendChild(cell);
+      }
+      board.appendChild(row);
+    }
+  };
+
+  return { initialLoad };
 };
 
 export { UI };
--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import { Player } from "./Player.js";
 import { UI } from "./UI.js";
+
+const mainUI = UI();
+
+mainUI.initialLoad();
